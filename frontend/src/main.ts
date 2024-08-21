607fc5a8b47898273a1fcb759c5ad8659a7e4eed
javigarmar08 <107317016+javigarmar08@users.noreply.github.com>
2024-08-21 04:31:55 +0200

    CSS changes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -18,15 +18,25 @@ import Panel from 'primevue/panel';
 import PickList from 'primevue/picklist';
 import DataTable from 'primevue/datatable';
 import Dropdown from 'primevue/dropdown';
+import Stepper from 'primevue/stepper';
+import StepperPanel from 'primevue/stepperpanel';
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import Column from 'primevue/column';
 import Avatar from 'primevue/avatar';
+import Calendar from 'primevue/calendar';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import RadioButton from 'primevue/radiobutton';
+import InputNumber from 'primevue/inputnumber';
+
+import Listbox from 'primevue/listbox';
+
 import AvatarGroup from 'primevue/avatargroup';   //Optional for grouping
 
 
-import { FaFlag, RiZhihuFill, GiTennisBall, FaUsers, HiSolidSearch, FaTrophy } from "oh-vue-icons/icons";
+import { FaFlag, RiZhihuFill, GiTennisBall, FaUsers, HiSolidSearch, FaTrophy, GiTennisCourt, HiSolidPlusSm, HiSolidMinusSm } from "oh-vue-icons/icons";
 
-addIcons(FaFlag, RiZhihuFill, GiTennisBall, FaUsers, FaTrophy, HiSolidSearch );
+addIcons(FaFlag, RiZhihuFill, GiTennisBall, FaUsers, FaTrophy, HiSolidSearch, GiTennisCourt, HiSolidPlusSm, HiSolidMinusSm);
 
 import 'primevue/resources/themes/aura-light-green/theme.css'
 
@@ -42,13 +52,21 @@ app.use(router)
 app.component('Button', Button) 
 app.component('DataView', DataView) 
 app.component('Panel', Panel) 
+app.component('Calendar', Calendar)
 app.component('Dropdown', Dropdown)
 app.component('Dialog', Dialog)
+app.component('Listbox', Listbox)
+app.component('Stepper', Stepper)
+app.component('StepperPanel', StepperPanel)
 app.component('InputText', InputText)
 app.component('PickList', PickList) 
 app.component('Avatar', Avatar) 
 app.component('v-icon', OhVueIcon) 
+app.component('InputNumber', InputNumber)
 app.component('DataTable', DataTable) 
+app.component('TabView', TabView)
+app.component('TabPanel', TabPanel)
+app.component('RadioButton', RadioButton)
 app.component('Column', Column) 
 app.component('AvatarGroup', AvatarGroup) 
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,21 +7,46 @@ import CompetitionsPanel from "@/components/competitions/CompetitionsPanel.vue";
 </script>
 
 <template>
-  <HeaderComponent />
-  <div class="players-container">
-    <PlayersPanel />
-    <MatchesPanel />
-    <CompetitionsPanel />   
-  </div>
+  <header>
+    <HeaderComponent />
+  </header>
+
+  <body>
+    <div class="panels-container">
+      <PlayersPanel class="panel"/>
+      <MatchesPanel />
+      <CompetitionsPanel />
+    </div>
+  </body>
+
 </template>
 
 <style scoped>
-.players-container {
-  padding-top: 2%;
+header {
+  line-height: 2;
+  max-height: 20vh;
+}
+
+body {
   display: flex;
-  flex-direction: row; /* Arrange items in a column */
-  gap: 16px; /* Add spacing between panels */
-  max-width: 100%;
-  overflow-x: auto;
+  flex-direction: column;
+  align-items: center;
+}
+
+/*  */
+
+.panels-container {
+  padding-top: 2%;
+  display: flex !important;
+  /* margin: 0 auto; */
+  justify-content: space-between;
+  
+  flex-direction: row;
+  /* Arrange items in a column */
+  gap: 16px;
+  /* Add spacing between panels */
+  max-width: 80%;
+  width: 100%;
+  /* overflow-x: auto; */
 }
 </style>
