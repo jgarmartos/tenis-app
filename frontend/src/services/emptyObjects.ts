23ce8ffa7c738e4c3763f8b4bc470d75551e8375
javigarmar08 <107317016+javigarmar08@users.noreply.github.com>
2024-09-01 03:19:05 +0200

    Updating emptyObject composable

--- frontend/src/services/emptyObjects.ts
+++ frontend/src/services/emptyObjects.ts
@@ -1,15 +1,22 @@
 import type { Place } from "@/interfaces/PlacesInterfaces";
+import type { Match } from "@/interfaces/MatchesInterfaces";
 
-const emptyMatch = () => ({
-    player1: 0,
-    player2: 0,
-    competition: 0,
-    place: 0,
-    date: new Date("2021-10-10T10:00:00"),
-    startTime: new Date("2021-10-10T10:00:00"),
-    endTime: new Date("2021-10-10T10:00:00"),
-    winner: null,
-});
+const emptyMatch = () => {
+    const match: Match = {
+        id: 0,
+        player1: emptyPlayer(),
+        player2: emptyPlayer(),
+        competition: emptyCompetition(),
+        place: emptyPlace(),
+        date: new Date("2021-10-10T10:00:00"),
+        startTime: new Date("2021-10-10T10:00:00"),
+        endTime: new Date("2021-10-10T10:00:00"),
+        winner: null,
+        surface: "",
+    }
+
+    return match;
+};
 
 const emptyPlayer = () => ({
     id: 0,
@@ -26,5 +33,13 @@ const emptyPlace = () => {
     };
     return place;
 };
+
+const emptyCompetition = () => ({
+    id: 0,
+    name: "",
+    startDate: new Date("2021-10-10T10:00:00"),
+    endDate: new Date("2021-10-10T10:00:00"),
+});
+
 
 export { emptyMatch, emptyPlayer, emptyPlace };
