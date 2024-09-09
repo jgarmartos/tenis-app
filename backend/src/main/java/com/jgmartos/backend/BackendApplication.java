64615977b8feb1ecc90c86e517ca9971e0be9c21
javigarmar08 <107317016+javigarmar08@users.noreply.github.com>
2024-09-09 19:19:23 +0200

    Updating cors to receive request from hytasa-tennis-db

--- backend/src/main/java/com/jgmartos/backend/BackendApplication.java
+++ backend/src/main/java/com/jgmartos/backend/BackendApplication.java
@@ -19,7 +19,10 @@ public class BackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all paths
-                        .allowedOrigins("http://localhost:3000", "https://master--lovely-gecko-25dc57.netlify.app") // Allow requests from your frontend
+                        .allowedOrigins("http://localhost:3000",
+                        "https://master--lovely-gecko-25dc57.netlify.app",
+                        "http://hytasa-tennis-db.es"
+                        ) // Allow requests from your frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Allow common HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies
