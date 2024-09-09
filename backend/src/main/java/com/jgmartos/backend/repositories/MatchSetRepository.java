0370cd57a449f381d806cee0522241bdda087ffc
javigarmar08 <107317016+javigarmar08@users.noreply.github.com>
2024-09-09 03:26:46 +0200

    Necessary configuration to deploy on Fly.io

--- backend/src/main/java/com/jgmartos/backend/repositories/MatchSetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jgmartos.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jgmartos.backend.models.MatchSet;
-
-public interface MatchSetRepository extends JpaRepository<MatchSet, Integer> {
-
-}
